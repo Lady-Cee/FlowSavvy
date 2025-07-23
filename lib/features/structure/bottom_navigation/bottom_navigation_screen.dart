bf2b6bf181c79801ae037acf2cb77a71c3f17eb6
Atinuke <aodeshola@yahoo.com>
2025-07-23 13:26:37 +0100

    feat: Add bottom_navigation_screen

---
+++ lib/features/structure/bottom_navigation/bottom_navigation_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flow_savvy/features/structure/educational/screen/educational_resource_screen.dart';
+import 'package:flow_savvy/features/structure/home/screen/home_screen.dart';
+import 'package:flow_savvy/features/structure/period/screen/period_log_screen.dart';
+import 'package:flow_savvy/features/structure/support/screen/community_support_screen.dart';
+import 'package:flow_savvy/features/structure/symptom/screen/symptom_log_screen.dart';
+import 'package:flow_savvy/features/structure/user/screen/user_profile_screen.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigationScreen extends StatefulWidget {
+  @override
+  _BottomNavigationScreenState createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    SymptomLogScreen(),
+    EducationalResourceScreen(),
+    UserProfileScreen(),
+    PeriodLogScreen(),
+    CommunitySupportScreen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        type: BottomNavigationBarType.shifting, // Needed for 5 items
+        backgroundColor: color.surface, // background color
+        selectedItemColor: color.primary, // selected icon & text color
+        unselectedItemColor: color.inversePrimary, // unselected icon & text color
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sign_language),
+            label: 'Symptom Log',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book_outlined),
+            label: 'Educational',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.water_drop),
+            label: 'PeriodLog',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Community Support',
+          ),
+        ],
+      ),
+    );
+  }
+}
